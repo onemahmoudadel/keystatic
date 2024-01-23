fe6e81bf2a45361de28be0abbd3fba5e179f17a6
= <=>
2024-01-23 21:52:10 +0200

    add netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,11 @@ import react from '@astrojs/react';
 import markdoc from '@astrojs/markdoc';
 import keystatic from '@keystatic/astro';
 
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), markdoc(), ...(process.env.SKIP_KEYSTATIC ? [] : [keystatic()])],
   output: 'hybrid',
+  adapter: netlify()
 });
